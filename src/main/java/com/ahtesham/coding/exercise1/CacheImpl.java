c9896a8d1a7a9308cefbf3e17aa64b2067b3ac48
alpinetundra <mailmeonwire@gmail.com>
2018-04-02 13:54:40 -0700

    cache last test pending

--- src/main/java/com/ahtesham/coding/exercise1/CacheImpl.java
+++ src/main/java/com/ahtesham/coding/exercise1/CacheImpl.java
@@ -1,21 +1,38 @@
 package com.ahtesham.coding.exercise1;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 public enum CacheImpl implements Cache<String, String>{
 
     INSTANCE{
-
+        private int size = 100;
         private Map<String, String> kvStore= new ConcurrentHashMap<>();
+
+        private Queue<String> keyList = new LinkedList<>();
 
         @Override
         public void put(String key, String value) {
+            if(kvStore.size() > size){
+                kvStore.remove(keyList.poll());
+            }
+            keyList.add(key);
             kvStore.put(key, value);
         }
 
         @Override
         public String get(String key) {
+            keyList.poll();
+            keyList.add(key);
             return kvStore.get(key);
+        }
+
+        public void changeSize(int newSize){
+            if(newSize > size){
+                size = newSize;
+            }
         }
     }
 }
--- src/test/java/com/ahtesham/coding/exercise1/CacheTest.java
+++ src/test/java/com/ahtesham/coding/exercise1/CacheTest.java
@@ -2,17 +2,38 @@ package com.ahtesham.coding.exercise1;
 
 
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.junit.runners.Suite;
-import static org.junit.Assert.assertEquals;
 
-@RunWith(Suite.class)
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@RunWith(JUnit4.class)
 public class CacheTest {
 
     @Test
     public void testAddToCache(){
+        CacheImpl.INSTANCE.put("A100","Jack Smith");
+        assertEquals("Jack Smith", CacheImpl.INSTANCE.get("A100"));
+    }
 
-        assertEquals("D", "D");
+    //test for maxsize;
+    @Test
+    public void testMaxCache(){
+        IntStream.of(110).forEach((i) -> CacheImpl.INSTANCE.put(String.valueOf(i), "same value"));
+        assertNull(CacheImpl.INSTANCE.get(String.valueOf("1")));
+    }
+    //test for synchronous threading
+
+    @Test
+    public void testSingleton(){
+       Thread t1 = new Thread(){
+
+       };
     }
 
 }
